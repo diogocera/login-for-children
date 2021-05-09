b0da8ec68611edb6a59eca53b7004d580c54bad7
Joana Bencatel <bencateljoana@gmail.com>
2021-05-09 15:30:56 +0100

    Create the App Nav Bar component

---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar app flat color="transparent">
+    <v-spacer />
+
+    <h5 v-if="userIsLoggedIn">{{ username }}</h5>
+    <app-nav-login-button v-else />
+  </v-app-bar>
+</template>
+
+<script>
+import AppNavLoginButton from "./AppNavLoginButton.vue";
+
+export default {
+  name: "AppNavBar",
+
+  components: { AppNavLoginButton },
+
+  computed: {
+    userIsLoggedIn() {
+      return this.$store.getters.isUserLoggedIn;
+    },
+
+    username() {
+      return this.$store.getters.getUsername;
+    },
+  },
+};
+</script>
+
+<style></style>
